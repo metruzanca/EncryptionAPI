22994000bf9814848d1c2daeb15a963aa4cd3e24
Samuele Zanca <samuele.zanca@mia-platform.eu>
2020-09-19 15:25:07 +0200

    debugging decrypt

--- api/decrypt.ts
+++ api/decrypt.ts
@@ -5,7 +5,12 @@ export default (request: NowRequest, response: NowResponse) => {
   const { message = null, key = process.env.SYMMETRIC_KEY } = request.query
   try {
     if(message === null) throw "message parameter required for decryption"
+    console.log(key);
+    
     let encrypted = decryptDecoded(message.toString(), key!.toString())
+
+    console.log(encrypted);
+
     response.status(200).send(encrypted.toString()) 
   } catch (error) {
     response.status(400).send("400 Malformed Request: " + error) 
